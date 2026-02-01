25da557ba46c4e048d0585a066cdc055750f382e
Henrique Leite <henrique.leite@aarin.com.br>
2026-02-01 12:25:21 -0300

    commit

--- infra/prod/using-ec2/ec2/init.sh
+++ infra/prod/using-ec2/ec2/init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-
-sudo sh ./get-docker.sh
-
-git clone https://github.com/henriquece/social-media.git
-
-cd social-media/infra/prod/using-ec2/docker-compose/
+cd /social-media/infra/prod/using-ec2/docker-compose/
 
 sudo docker compose up server-http
 
 sudo docker compose run --rm certbot -v certonly --webroot --webroot-path /var/www/certbot/ -d api.henriquece.dev
+
+sudo docker compose down
+
+sudo docker compose up server-https
